6dde5385759963cbd52220258d86645841b337b8
dongduu <4012popo@naver.com>
2021-10-29 02:07:09 +0900

    [스타트업트랙 프리코스] add : JS 시작하기 - 삼항연산자

--- js/main2.js
+++ js/main2.js
@@ -1,37 +1,11 @@
-// 비교 연산자(comparison operator)
+// 삼항 연산자(ternaty operator)
 
-const a = 1;
-const b = 1;
+const a = 1 < 2;
 
-console.log(a === b);
-
-function isEqual (x, y) {
-    return x === y;
+if (a) {
+    console.log('참');
+} else {
+    console.log('거짓');
 }
 
-console.log(isEqual(1, 2));
-console.log(isEqual(1, '2'));
-
-const c = 1;
-const d = 1;
-
-console.log(c !== d);
-console.log(c < d);
-console.log(c > d);
-console.log(c <= d);
-console.log(c >= d);
-
-// 논리 연산자(logical operator)
-
-const e = 1 === 1;
-const f = 'ab' === 'ab';
-const g = false;
-
-console.log(e);
-console.log(f);
-console.log(g);
-
-console.log('&&: ', e && f && g);
-console.log('||: ', e || f || g);
-console.log('!: ', e);
-console.log('!: ', !e);
+console.log(a ? '참' : '거짓');
