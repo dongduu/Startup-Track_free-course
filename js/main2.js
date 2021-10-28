7aec84d7b2e2d78f1b3a833163f37f669be98a09
dongduu <4012popo@naver.com>
2021-10-28 21:39:26 +0900

    [스타트업트랙 프리코스] add : JS 시작하기 - (4)

---
+++ js/main2.js
@@ -0,0 +1,17 @@
+// 산술 연산자(arithmetic operator)
+
+console.log(1 + 2);
+console.log(5 - 2);
+console.log(1 + 2);
+console.log(10 / 2);
+console.log(10 % 3);
+
+// 할당 연산자(assignment operator)
+
+const a = 1; // 재할당 불가능
+
+let b = 2; // 재할당 가능
+b += 1 // b = b + 1;
+
+console.log(b);
+
