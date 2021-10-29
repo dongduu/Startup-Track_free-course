3855eb4512be152762d34dde46c3c0dcd725ea5c
dongduu <4012popo@naver.com>
2021-10-30 00:10:23 +0900

    [스타트업트랙 프리코스] add : JS 함수 - 함수 복습

--- js/main2.js
+++ js/main2.js
@@ -1,17 +1,35 @@
-// 형 변환(Type conversion)
+// 함수 복습
 
-const a = 1;
-const b = '1';
-
-console.log(a === b);
-console.log(a == b);
-
-// Truthy (참 같은 것)
-// true, {}, [], 1, 2, 'false', -12, '3.14'...
-
-// Falsy (거짓 같은 것)
-// false, '', null, undifined, 0, -0, NaN
-
-if (true) {
-    console.log(123);
+function sum (x, y) { 
+    return x + y
+    console.log(x);
 }
+
+const a = sum(1, 2); 
+const b = sum(12, 43);
+
+console.log(a);
+console.log(b);
+console.log(a + b);
+
+const sum2 = function (x, y) { 
+    return x + y
+}
+
+sum2(3, 4);
+
+function sum3 (x, y) { 
+    if (x < 2) {
+        return;
+    }
+    return x + y
+}
+
+console.log(sum3(1, 3));
+
+function sum4 () { 
+    console.log(arguments);
+    return arguments[0] + arguments[1];
+}
+
+console.log(sum4(12, 12));
