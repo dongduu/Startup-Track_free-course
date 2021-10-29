13c260efda7f892a693856c8545c0f8c0f3776b1
dongduu <4012popo@naver.com>
2021-10-29 20:56:07 +0900

    [스타트업트랙 프리코스] add : JS 시작하기 - 형 변환

--- js/main2.js
+++ js/main2.js
@@ -1,11 +1,17 @@
-// 변수 유효범위(Variable Scope)
-// var, let, const
+// 형 변환(Type conversion)
 
-function scope () {
-    if (true) {
-        const a = 123;
-        console.log(a);
-    }
+const a = 1;
+const b = '1';
+
+console.log(a === b);
+console.log(a == b);
+
+// Truthy (참 같은 것)
+// true, {}, [], 1, 2, 'false', -12, '3.14'...
+
+// Falsy (거짓 같은 것)
+// false, '', null, undifined, 0, -0, NaN
+
+if (true) {
+    console.log(123);
 }
-
-scope();
