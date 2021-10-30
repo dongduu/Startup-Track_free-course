e77c4b9b73b6c64d13837868e5c6ef5884444254
dongduu <4012popo@naver.com>
2021-10-30 23:23:09 +0900

    [스타트업트랙 프리코스] add : JS 함수 - 호이스팅

--- js/main2.js
+++ js/main2.js
@@ -1,13 +1,16 @@
-// 즉시실행함수
-// IIFE(Immediately-Invoked Function Expression)
+// 호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
 
-const a = 7;
+const a = 3;
 
-function double () {
+double ();
+
+const double = function () {
     console.log(a * 2);
 }
-double();
 
-(function () {
+double2 ();
+
+function double2 () {
     console.log(a * 2);
-}) ()
+}
