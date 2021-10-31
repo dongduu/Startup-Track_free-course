f9bd164636f22898a5052243be37ddb58ee1d607
dongduu <4012popo@naver.com>
2021-10-31 21:42:15 +0900

    [스타트업트랙 프리코스] add : JS 함수 - callback

--- js/main2.js
+++ js/main2.js
@@ -1,23 +1,16 @@
-// 타이머 함수
-// setTimeout(함수, 시간): 일정 시간 후 함수 실행
-// setInterval(함수, 시간): 시간 간격마다 함수 실행
-// clearTimeout(): 설정된 Timeout 함수를 종료
-// clearInterval(): 설정된 Interval 함수를 종료
+// 콜백(callback)
+// 함수의 인수로 사용되는 함수
 
-const timer = setTimeout(() => {
-    console.log('hi');
-}, 3000)
+// setTimeout(함수, 시간)
 
-// const h1El = document.querySelector('h1');
-// h1El.addEventListener('click', () => {
-//     clearTimeout(timer);
-// })
+function timeout (callback) {
+    setTimeout(() => {
+        console.log('hi');
+        callback();
+    }, 3000);
+}
 
-const timer2 = setInterval(() => {
-    console.log('bye');
-}, 3000)
+timeout(() => {
+    console.log('done');
+});
 
-const h1El = document.querySelector('h1');
-h1El.addEventListener('click', () => {
-    clearInterval(timer2);
-})
