cdfd99f4761ebe3767e547cf00b5982e7af66004
dongduu <4012popo@naver.com>
2021-11-02 00:21:35 +0900

    [스타트업트랙 프리코스] add : JS 클래스 - ES6 classes

--- js/main2.js
+++ js/main2.js
@@ -1,13 +1,38 @@
 // ES6 classes
 
-const heropy = {
-    name: 'Heropy',
-    normal () { // function 생략 가능
-        console.log(this.name);
-    },
-    arrow: () => {
-        console.log(this.name);
+// const heropy = {
+//     name: 'Heropy',
+//     normal () { // function 생략 가능
+//         console.log(this.name);
+//     },
+//     arrow: () => {
+//         console.log(this.name);
+//     }
+// }
+// heropy.normal();
+// heropy.arrow();
+
+class User {
+    constructor(first, last) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
     }
 }
-heropy.normal();
-heropy.arrow();
+
+// function User (first, last) {
+//     this.firstName = first;
+//     this.lastName = last;
+// }
+// User.prototype.getFullName = function () {
+//     return `${this.firstName} ${this.lastName}`
+// }
+
+const heropy = new User('Heropy', 'park');
+const amy = new User('Amy', 'Lee');
+const neo = new User('Neo', 'Kim');
+
+console.log(heropy);
+console.log(amy.getFullName());
