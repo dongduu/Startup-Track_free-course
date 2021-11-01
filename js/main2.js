53e04569ce9b2d1f544163ee5c5a920c826de85d
dongduu <4012popo@naver.com>
2021-11-01 23:19:43 +0900

    [스타트업트랙 프리코스] add : JS 클래스 - ES6 classes

--- js/main2.js
+++ js/main2.js
@@ -1,10 +1,8 @@
-// this
-// 일반(Nomal) 함수는 호출 위치에서 따라 this 정의!
-// 화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의!
+// ES6 classes
 
 const heropy = {
     name: 'Heropy',
-    normal: function () {
+    normal () { // function 생략 가능
         console.log(this.name);
     },
     arrow: () => {
@@ -13,31 +11,3 @@ const heropy = {
 }
 heropy.normal();
 heropy.arrow();
-
-const amy = {
-    name: 'amy',
-    normal: heropy.normal,
-    arrow: heropy.arrow
-}
-amy.normal();
-amy.arrow();
-
-const timer = {
-    name:'dongdu',
-    timeout: function () {
-        setTimeout(function () {
-            console.log(this.name);
-        }, 2000);
-    }
-}
-timer.timeout();
-
-const timer2 = {
-    name:'dongdu',
-    timeout: function () {
-        setTimeout(() => {
-            console.log(this.name);
-        }, 2000);
-    }
-}
-timer2.timeout();
